9062f36b10c024ab1a6902927a9c594cc46e53dd
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-12-04 18:40:41 +0000

    Import TodoItem component into Todos, map over todos and output titles

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem.js';
 
 class Todos extends Component {
 
   render() {
     return this.props.todos.map((todo) => (
-      <h3>{todo.title}</h3>
+      <TodoItem todo={todo} />
     ));
   }
 }
